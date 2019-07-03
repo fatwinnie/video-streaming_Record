514b987713703e534028e9af00ba56e92012c448
fatwinnie <49060668+fatwinnie@users.noreply.github.com>
2019-07-03 22:38:59 +0800

    update video record when ROI
    在ROI時video record ，加上定義size (定義擷取的尺寸) 
    才能播放影片檔案

--- camera.py
+++ camera.py
@@ -8,17 +8,23 @@ class RecordingThread (threading.Thread):
         self.isRunning = True
         self.cap = camera
         fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-        self.out = cv2.VideoWriter('./static/video.avi',fourcc, 20.0, (640,480))
+        size = (500,300)  #寬500,高300(width,height)
+        self.out = cv2.VideoWriter('./static/video.avi',fourcc,60.0,size)
+        #self.out = cv2.VideoWriter('./static/video.avi',fourcc, 20.0, (640,480))
 
     def run(self):
         while self.isRunning:
             ret, frame = self.cap.read()
-            gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+            #gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+            roi = frame[0:300, 0:500] #寬500,高300
+            gray = cv2.cvtColor(roi,cv2.COLOR_BGR2GRAY)
+            
             #再轉換一次灰階到彩色才會有三通到，儲存影片才能成功
             color = cv2.cvtColor(gray,cv2.COLOR_GRAY2RGB)
             if ret:
                 self.out.write(color)
-                #self.out.write(frame)
+                #self.out.write(roi)
+                
                 
 
         self.out.release()
@@ -47,26 +53,10 @@ class VideoCamera(object):
     def get_frame(self):
         ret, frame = self.cap.read()
         gray = cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)
+        ROI= gray[0:int(300), 0:int(500)] #寬500,高300
 
         if ret:
-            ret, jpeg = cv2.imencode('.jpg', gray)
-            
-            
-
-            # Record video
-            # if self.is_record:
-            #     if self.out == None:
-            #         fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-            #         self.out = cv2.VideoWriter('./static/video.avi',fourcc, 20.0, (640,480))
-                
-            #     ret, frame = self.cap.read()
-            #     if ret:
-            #         self.out.write(frame)
-            # else:
-            #     if self.out != None:
-            #         self.out.release()
-            #         self.out = None  
-
+            ret, jpeg = cv2.imencode('.jpg', ROI)
             return jpeg.tobytes()
       
         else:
