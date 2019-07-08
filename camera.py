03f1aec42e3abaee7fc5d3a921e25e7b0866f068
fatwinnie <49060668+fatwinnie@users.noreply.github.com>
2019-07-08 16:33:49 +0800

    Add record save to image.jpg file
    Add record save to image.jpg file

--- camera.py
+++ camera.py
@@ -1,5 +1,9 @@
 import cv2
+import os
 import threading
+from PIL import Image
+from timeit import default_timer as timer
+
 
 class RecordingThread (threading.Thread):
     def __init__(self, name, camera):
@@ -9,21 +13,27 @@ class RecordingThread (threading.Thread):
         self.cap = camera
         fourcc = cv2.VideoWriter_fourcc(*'MJPG')
         size = (500,300)  #寬500,高300(width,height)
-        self.out = cv2.VideoWriter('./static/video.avi',fourcc,60.0,size)
+        self.out = cv2.VideoWriter('./static/video.avi',fourcc,15,size)
         #self.out = cv2.VideoWriter('./static/video.avi',fourcc, 20.0, (640,480))
 
     def run(self):
+        frmae_counter = 0
         while self.isRunning:
             ret, frame = self.cap.read()
+            flipframe = cv2.flip(frame,0) #垂直翻轉
             #gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-            roi = frame[0:300, 0:500] #寬500,高300
-            gray = cv2.cvtColor(roi,cv2.COLOR_BGR2GRAY)
-            
-            #再轉換一次灰階到彩色才會有三通到，儲存影片才能成功
-            color = cv2.cvtColor(gray,cv2.COLOR_GRAY2RGB)
-            if ret:
-                self.out.write(color)
-                #self.out.write(roi)
+            roi = flipframe[0:300, 0:500] #寬500,高300
+            gray = cv2.cvtColor(roi,cv2.COLOR_BGR2GRAY)               
+            color = cv2.cvtColor(gray,cv2.COLOR_GRAY2RGB)  #再轉換一次灰階到彩色才會有三通到，儲存影片才能成功
+            target = Image.fromarray(cv2.cvtColor(color, cv2.COLOR_BGR2RGB))
+            if frmae_counter <= 30:
+                if ret:
+                    target.save(os.path.join('./static',str(frmae_counter) + '.jpg'))
+                    frmae_counter += 1
+                    self.out.write(color)
+                    #self.out.write(roi)
+            else:
+                self.isRunning = False
                 
                 
 
@@ -52,11 +62,13 @@ class VideoCamera(object):
     
     def get_frame(self):
         ret, frame = self.cap.read()
-        gray = cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)
-        ROI= gray[0:int(300), 0:int(500)] #寬500,高300
+        flipped = cv2.flip(frame,0) #垂直翻轉
+        #gray = cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)
+        #ROI= gray[0:int(300), 0:int(500)] #寬500,高300
 
         if ret:
-            ret, jpeg = cv2.imencode('.jpg', ROI)
+            #ret, jpeg = cv2.imencode('.jpg', ROI)
+            ret, jpeg = cv2.imencode('.jpg', flipped)
             return jpeg.tobytes()
       
         else:
@@ -72,5 +84,3 @@ class VideoCamera(object):
 
         if self.recordingThread != None:
             self.recordingThread.stop()
-
-            
